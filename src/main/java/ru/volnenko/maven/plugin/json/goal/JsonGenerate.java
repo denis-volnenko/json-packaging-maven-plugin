18d3e2a7d27998ff8836f95693bbc588ca6df9b0
Denis Volnenko <denis@volnenko.ru>
2025-04-15 12:35:32 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/json/goal/JsonGenerate.java
+++ src/main/java/ru/volnenko/maven/plugin/json/goal/JsonGenerate.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
 import ru.volnenko.maven.plugin.json.parser.RootParser;
 
 import java.io.File;
@@ -30,11 +30,36 @@ public class JsonGenerate extends AbstractMojo {
     @Parameter(property = "files")
     private List<String> files = new ArrayList<>();
 
+    @Parameter(defaultValue = "${settings}", required = true, readonly = true)
+    private Settings settings;
+
     @Override
     @SneakyThrows
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() {
         @NonNull final File buildPath = new File(project.getBuild().getDirectory());
         buildPath.mkdirs();
+
+        for (final Object dependencyObject: project.getDependencyArtifacts()) {
+            if (dependencyObject == null) continue;
+            final DefaultArtifact dependency = (DefaultArtifact) dependencyObject;
+            if (!"compile".equalsIgnoreCase(dependency.getScope())) continue;
+            if ("json".equalsIgnoreCase(dependency.getType()) || "yaml".equalsIgnoreCase(dependency.getType())) {
+                System.out.println(dependency);
+                final String name = dependency.getGroupId().replace(".", "/") + "/"
+                        + dependency.getArtifactId() + "/" + dependency.getVersion() + "/"
+                        + dependency.getArtifactId() + "-" + dependency.getVersion() + ".json";
+                final File file = new File(settings.getLocalRepository(), name);
+                final String filename = file.getAbsolutePath();
+                if (!file.exists()) {
+                    System.err.println("Error! File `"+ filename + "` is not exists...");
+                    continue;
+                }
+                if (!files.contains(file.getAbsolutePath())) {
+                    files.add(filename);
+                    System.out.println("ADD:" + file);
+                }
+            }
+        }
 
         @NonNull final String sourceNameJSON = project.getBuild().getFinalName() + "." + project.getPackaging();
         @NonNull final File build = new File(project.getBuild().getDirectory(), sourceNameJSON);
