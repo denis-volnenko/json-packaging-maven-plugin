122a82cd6fbb0c9415b16fa9e41c8239a258010e
Denis Volnenko <denis@volnenko.ru>
2025-04-15 08:49:51 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/json/JsonGenerate.java
+++ src/main/java/ru/volnenko/maven/plugin/json/JsonGenerate.java
@@ -1,32 +1,37 @@
 package ru.volnenko.maven.plugin.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-@Mojo(name = "package", defaultPhase = LifecyclePhase.PACKAGE)
-public class JsonPackage extends AbstractMojo {
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+public class JsonGenerate extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
     @Override
     @SneakyThrows
-    public void execute() {
+    public void execute() throws MojoExecutionException, MojoFailureException {
         final File buildPath = new File(project.getBuild().getDirectory());
         buildPath.mkdirs();
 
         final String sourceNameJSON = project.getBuild().getFinalName() + "." + project.getPackaging();
         final File build = new File(project.getBuild().getDirectory(), sourceNameJSON);
 
-        final Artifact artifact =  project.getArtifact();
-        artifact.setFile(build);
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Map<String, Object> map = new LinkedHashMap<>();
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(build, map);
     }
 
 }
