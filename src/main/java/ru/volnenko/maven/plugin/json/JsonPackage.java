7dffd359dc42da771f14d173808eac14bcda45e3
Denis Volnenko <denis@volnenko.ru>
2025-04-15 08:45:31 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/json/JsonPackage.java
+++ src/main/java/ru/volnenko/maven/plugin/json/JsonPackage.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -23,7 +21,7 @@ public class JsonPackage extends AbstractMojo {
 
     @Override
     @SneakyThrows
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() {
         System.out.println("[ PACKAGE ]");
         final File buildPath = new File(project.getBuild().getDirectory());
         buildPath.mkdirs();
