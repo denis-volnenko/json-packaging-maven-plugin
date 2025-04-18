6815edd2673e23b514c1bc10f6b6879236174d1d
Denis Volnenko <denis@volnenko.ru>
2025-04-18 10:02:11 +0400

    Project updated.

--- src/main/java/ru/volnenko/maven/plugin/json/goal/JsonGenerate.java
+++ src/main/java/ru/volnenko/maven/plugin/json/goal/JsonGenerate.java
@@ -44,9 +44,13 @@ public class JsonGenerate extends AbstractMojo {
             final DefaultArtifact dependency = (DefaultArtifact) dependencyObject;
             if (!"compile".equalsIgnoreCase(dependency.getScope())) continue;
             if ("json".equalsIgnoreCase(dependency.getType()) || "yaml".equalsIgnoreCase(dependency.getType())) {
+                String classifier = dependency.getClassifier();
+                if (classifier == null) classifier = "";
+                else classifier = "-" + classifier;
                 final String name = dependency.getGroupId().replace(".", "/") + "/"
                         + dependency.getArtifactId() + "/" + dependency.getVersion() + "/"
-                        + dependency.getArtifactId() + "-" + dependency.getVersion() + "." + dependency.getType();
+                        + dependency.getArtifactId() + "-" + dependency.getVersion() + classifier
+                        + "." + dependency.getType();
                 final File file = new File(settings.getLocalRepository(), name);
                 final String filename = file.getAbsolutePath();
                 if (!file.exists()) {
